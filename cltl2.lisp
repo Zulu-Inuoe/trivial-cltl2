85de4cbb3156bd28505a11876238d33b8b56c3d0
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2018-01-16 20:02:58 -0500

    Make consistent use of uninterned symbols vs keywords

--- cltl2.lisp
+++ cltl2.lisp
@@ -1,18 +1,18 @@
-(in-package :cl-user)
+(in-package #:cl-user)
 
 #+sbcl
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require "sb-cltl2"))
 
-(defpackage :trivial-cltl2
-  (:nicknames :cltl2)
-  (:use :cl
-        #+sbcl :sb-cltl2
-        #+openmcl :ccl
-        #+cmu :ext
-        #+allegro :sys
-        #+ecl :si
-        #+abcl :lisp)
+(defpackage #:trivial-cltl2
+  (:nicknames #:cltl2)
+  (:use #:cl
+        #+sbcl #:sb-cltl2
+        #+openmcl #:ccl
+        #+cmu #:ext
+        #+allegro #:sys
+        #+ecl #:si
+        #+abcl #:lisp)
   (:export #:compiler-let
            #:variable-information
            #:function-information
