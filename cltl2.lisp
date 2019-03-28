1a34cab59a980c758cbd0ee929df506b0bb8e4c5
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-03-29 01:00:15 +0900

    allegro fix

--- cltl2.lisp
+++ cltl2.lisp
@@ -13,6 +13,7 @@
         #+allegro #:sys
         #+ecl #:si
         #+abcl #:lisp)
+  #+allegro (:import-from #:excl #:compiler-let)
   (:export #:compiler-let
            #:variable-information
            #:function-information
@@ -21,3 +22,16 @@
            #:define-declaration
            #:parse-macro
            #:enclose))
+
+(in-package #:trivial-cltl2)
+
+;;; This code is derived from 'clweb' by Alex Plotnick,
+;;; https://github.com/plotnick/clweb/blob/4c736b4c8b4c0afbdd939eefbcb986c16c24c1e3/clweb.lisp#L1853
+#+allegro
+(defun parse-macro (name lambda-list body &optional env)
+  (declare (ignorable name lambda-list body env))
+  (excl::defmacro-expander `(,name ,lambda-list ,@body) env))
+
+#+allegro
+(defun enclose (lambda-expression &optional environment)
+  (excl:compile-lambda-expr-in-env lambda-expression environment))
