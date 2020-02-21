7e40f04d2e5b727f32082e2833c235915ceef574
Yokota Yuki <y2q-actionman@users.noreply.github.com>
2020-02-21 14:44:09 +0900

    Allegro compatibility layer for variable-information, function-information, define-declaration (partial)

--- cltl2.lisp
+++ cltl2.lisp
@@ -8,11 +8,13 @@
         #+sbcl #:sb-cltl2
         #+openmcl #:ccl
         #+cmu #:ext
-        #+allegro #:sys
         #+ecl #:si
         #+abcl #:lisp
         #+lispworks #:hcl)
   #+allegro (:import-from #:excl #:compiler-let)
+  #+allegro (:import-from #:system
+                          #:declaration-information
+                          #:augment-environment)
   #+lispworks (:import-from #:lw #:compiler-let)
   (:export #:compiler-let
            #:variable-information
@@ -24,6 +26,58 @@
            #:enclose))
 
 (in-package #:trivial-cltl2)
+
+#+allegro
+(defun allegro-information-alist-kludge (alist &optional environment) ; XXX
+  "Allegro 10.1 returns a type specifier IN A LIST, like '((ARRAY (INTEGER 0 1) (*))))'.
+So I unwrap it.."
+  (flet ((valid-type-specifier-p (spec)
+           (ignore-errors (subtypep spec spec environment))))
+    (let* ((type-spec (cdr (assoc 'type alist))))
+      (unless (valid-type-specifier-p type-spec)
+        (assert (and (consp type-spec)
+                     (= (length type-spec) 1)
+                     (valid-type-specifier-p (first type-spec)))
+                ()
+                "Unexpected format type-specifier: ~A" type-spec)
+        (push (cons 'type (first type-spec))
+              alist)))
+    alist))
+
+#+allegro
+(defun variable-information (symbol &optional environment all-declarations)
+  "Compatibility layer for Allegro's `system:variable-information',
+which takes extra parameters and returns different values from the CLtL2's function."
+  ;; See https://franz.com/support/documentation/current/doc/operators/system/variable-information.htm
+  (multiple-value-bind (type locative-cons alist local-p)
+      (system:variable-information symbol environment all-declarations)
+    (values type
+            local-p
+            (allegro-information-alist-kludge alist)
+            locative-cons)))
+
+#+allegro
+(defun function-information (fspec &optional environment all-declarations special-operators)
+  "Compatibility layer for Allegro's `system:function-information',
+which returns different values from the CLtL2's function."
+  ;; See https://franz.com/support/documentation/current/doc/operators/system/function-information.htm
+  (multiple-value-bind (type locative-cons alist local-p)
+      (system:function-information fspec environment all-declarations special-operators)
+    (values (case type
+              (:special-operator :special-form)
+              (otherwise type)) 
+            local-p
+            (allegro-information-alist-kludge alist)
+            locative-cons)))
+
+#+allegro
+(defmacro define-declaration (decl-name lambda-list &body body)
+  "Compatibility layer for Allegro's `system:function-information',
+which has very different arguments and return values."
+  `(system:define-declaration ,decl-name ,lambda-list
+     ,decl-name                         ; 'prop' argument.
+     :both                              ; 'kind' argument. TODO: guess
+     (lambda (decl-name env) ,@body)))  ; 'def' argument.
 
 ;;; This code is derived from 'clweb' by Alex Plotnick,
 ;;; https://github.com/plotnick/clweb/blob/4c736b4c8b4c0afbdd939eefbcb986c16c24c1e3/clweb.lisp#L1853
